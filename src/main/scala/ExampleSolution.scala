1b3e0ccc38c035e01827e8a915f39b790fd76dd2
MichalPorazko <michal.porazko@gmail.com>
2024-09-30 22:49:32 +0200

    Just gettin started

--- src/main/scala/ExampleSolution.scala
+++ src/main/scala/ExampleSolution.scala
@@ -1,66 +1,69 @@
-type Card = Char
-type Hand = String
-
-case class Bet(hand: Hand, bid: Int)
-object Bet:
-  def apply(s: String): Bet = Bet(s.take(5), s.drop(6).toInt)
-
-enum HandType:
-  case HighCard, OnePair, TwoPair, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind
-object HandType:
-  def apply(hand: Hand)(using rules: Rules): HandType =
-    val cardCounts: Map[Card, Int] =
-      hand.groupBy(identity).mapValues(_.length).toMap
-
-    val cardGroups: List[Int] = rules.wildcard match
-      case Some(card) if cardCounts.keySet.contains(card) =>
-        val wildcardCount = cardCounts(card)
-        val cardGroupsNoWildcard = cardCounts.removed(card).values.toList.sorted.reverse
-        cardGroupsNoWildcard match
-          case Nil => List(wildcardCount)
-          case _ => cardGroupsNoWildcard.head + wildcardCount :: cardGroupsNoWildcard.tail
-      case _ => cardCounts.values.toList.sorted.reverse
-
-    cardGroups match
-      case 5 :: _ => HandType.FiveOfAKind
-      case 4 :: _ => HandType.FourOfAKind
-      case 3 :: 2 :: Nil => HandType.FullHouse
-      case 3 :: _ => HandType.ThreeOfAKind
-      case 2 :: 2 :: _ => HandType.TwoPair
-      case 2 :: _ => HandType.OnePair
-      case _ => HandType.HighCard
-  end apply
-end HandType
-
-trait Rules:
-  val rankValues: String
-  val wildcard: Option[Card]
-
-val standardRules = new Rules:
-  val rankValues = "23456789TJQKA"
-  val wildcard = None
-
-val jokerRules = new Rules:
-  val rankValues = "J23456789TQKA"
-  val wildcard = Some('J')
 
 
-given cardOrdering(using rules: Rules): Ordering[Card] = Ordering.by(rules.rankValues.indexOf(_))
-given handOrdering(using Rules): Ordering[Hand] = (h1: Hand, h2: Hand) =>
-  val h1Type = HandType(h1)
-  val h2Type = HandType(h2)
-  if h1Type != h2Type then h1Type.ordinal - h2Type.ordinal
-  else h1.zip(h2).find(_ != _).map( (c1, c2) => cardOrdering.compare(c1, c2) ).getOrElse(0)
-given betOrdering(using Rules): Ordering[Bet] = Ordering.by(_.hand)
 
-def calculateWinnings(bets: List[Bet])(using Rules): Int =
-  bets.sorted.zipWithIndex.map { case (bet, index) => bet.bid * (index + 1) }.sum
 
-def parse(input: String): List[Bet] =
-  input.linesIterator.toList.map(Bet(_))
+   def partOne(input: Seq[String]): Int =
+    input.map(parseInput(1, _)).sorted.zipWithIndex.map((hand, idx) => hand.bid * (idx + 1)).sum
 
-def part1(input: String): Unit =
-  println(calculateWinnings(parse(input))(using standardRules))
+   def partTwo(input: Seq[String]): Int =
+    input.map(parseInput(2, _)).sorted.zipWithIndex.map((hand, idx) => hand.bid * (idx + 1)).sum
 
-def part2(input: String): Unit =
-  println(calculateWinnings(parse(input))(using jokerRules))
+  lazy val cardMapPart1: Map[Char, Int] =
+    Seq('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2').reverse.zipWithIndex.toMap
+
+  lazy val cardMapPart2: Map[Char, Int] =
+    Seq('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J').reverse.zipWithIndex.toMap
+
+  enum HandType(val id: Int):
+    case FiveOfAKind  extends HandType(id = 6)
+    case FourOfAKind  extends HandType(id = 5)
+    case FullHouse    extends HandType(id = 4)
+    case ThreeOfAKind extends HandType(id = 3)
+    case TwoPairs     extends HandType(id = 2)
+    case OnePair      extends HandType(id = 1)
+    case HighCard     extends HandType(id = 0)
+  end HandType
+
+  lazy val determineTypePartOne: Map[Int, Int] => HandType = map =>
+    val counts = map.values.toSeq
+    if counts contains 5 then HandType.FiveOfAKind
+    else if counts contains 4 then FourOfAKind
+    else if (counts contains 2) && (counts contains 3) then FullHouse
+    else if counts contains 3 then ThreeOfAKind
+    else if counts.count(_ == 2) == 2 then TwoPairs
+    else if counts contains 2 then OnePair
+    else if !counts.exists(_ != 1) then HighCard
+    else throw new IllegalArgumentException("Could not determine hand type")
+
+  lazy val determineTypePartTwo: Map[Int, Int] => HandType = counts =>
+    val jokers = counts.getOrElse(0, 0)
+    determineTypePartOne(counts) match
+      case FiveOfAKind  => FiveOfAKind
+      case FourOfAKind  => if jokers >= 1 then FiveOfAKind else FourOfAKind
+      case FullHouse    => if jokers != 0 then FiveOfAKind else FullHouse
+      case ThreeOfAKind => if jokers != 0 then FourOfAKind else ThreeOfAKind
+      case TwoPairs     => if jokers == 0 then TwoPairs else if jokers == 1 then FullHouse else FourOfAKind
+      case OnePair      => if jokers == 0 then OnePair else ThreeOfAKind
+      case HighCard     => if jokers > 0 then OnePair else HighCard
+
+  lazy val countCards: Seq[Int] => Map[Int, Int] = _.foldLeft(Map.empty[Int, Int]):
+    case (counts, card) => counts.updatedWith(card)(value => Some(value.getOrElse(0) + 1))
+
+  case class Hand(cards: Seq[Int], bid: Int, handType: HandType) extends Ordered[Hand]:
+    override def compare(that: Hand): Int = this.handType.id compareTo that.handType.id match
+      case 0 => this.cards.indices
+        .collectFirst {
+          case idx if (this.cards(idx) compareTo that.cards(idx)) != 0 => this.cards(idx) compareTo that.cards(idx)
+        }
+        .getOrElse(0)
+      case x => x
+  end Hand
+
+  def parseInput(part: Int, line: String): Hand =
+    val (cardMap, eval) =
+      if part == 1 then (cardMapPart1, determineTypePartOne) else (cardMapPart2, determineTypePartTwo)
+    val (cards, bid)    = line.span(!_.isWhitespace)
+    val cardNumbers     = cards.flatMap(cardMap.get)
+    val cardCounts      = countCards(cardNumbers)
+
+    Hand(cards = cardNumbers, bid = bid.strip.toInt, handType = eval(cardCounts))
